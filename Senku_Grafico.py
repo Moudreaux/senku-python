f65ad91c011b006fb3b40647dde7dc88e7f94407
Leonardo <moudreaux@hotmail.com>
2019-07-24 23:04:14 -0300

    Tercera Versión Gráfica Incluye botones para reiniciar y salir.

--- Senku_Grafico.py
+++ Senku_Grafico.py
@@ -1,25 +1,5 @@
 import pygame as pg
 
-pg.init()
-
-fondo = pg.image.load('fondo.png')
-
-# fichas
-anchoficha = 60
-altoficha = 60
-x = pg.image.load('x.png')
-o = pg.image.load('o.png')
-Y = pg.image.load('Y.png')
-esp = pg.image.load('esp.png')
-# textos
-fuente36 = pg.font.Font('8-BIT-WONDER.TTF', 36)
-fuente24 = pg.font.Font('8-BIT-WONDER.TTF', 24)
-
-# Colores
-Negro = (0, 0, 0)
-Amarillo = (206, 206, 0)
-Violeta = (206, 0, 206)
-
 
 class Ficha:
     def __init__(self, valor, fil, col):
@@ -33,7 +13,7 @@ class Ficha:
 
 
 class Tabla:
-    def __init__(self, t):
+    def __init__(self, t, p):
         tab = []
         fila = 0
         for linea in t.split(','):
@@ -54,23 +34,23 @@ class Tabla:
         self.anterior = ""
         self.fichas = len(t) - len(t.replace('x', ''))
         self.comidas = 0
-        self.puntaje = 0
+        self.puntaje = p
 
     def redibujar(self):
         def valor(t: str, y: int):
-            x = 910
+            x = 940
             texto = fuente36.render(t, True, Amarillo)
             ancho = texto.get_width()
             pantalla.blit(texto, (x - ancho, y))
-            pg.draw.rect(pantalla, Negro, (x - ancho - 2, y + 20, texto.get_width() + 4, 3))
+            pg.draw.rect(pantalla, Negro, (x - ancho - 2, y + 18, texto.get_width() + 4, 5))
 
-        def titulo(t: str, y: int, sub: bool = True, ):
+        def titulo(t: str, y: int):
             x = 720
             texto = fuente36.render(t, True, Amarillo)
             ancho = texto.get_width()
             pantalla.blit(texto, (x, y))
             pg.draw.rect(pantalla, Negro, (x - 2, y + 15, ancho + 4, 5))
-            if sub: pg.draw.rect(pantalla, Violeta, (x, y + texto.get_height() + 2, 260, 10))
+            pg.draw.rect(pantalla, Violeta, (x, y + texto.get_height() + 2, 260, 10))
 
         pantalla.blit(fondo, (0, 0))
         for l in self.t:
@@ -86,8 +66,14 @@ class Tabla:
         valor(str(self.comidas), 525)
         titulo("NIVEL ", 580)
         valor(str(nivel), 635)
+        pantalla.blit(reiniciar, (740, 700))
+        if sonido != 0:
+            pantalla.blit(ruido, (830, 700))
+        else:
+            pantalla.blit(silencio, (830, 700))
+        pantalla.blit(salida, (920, 700))
 
-    def mover(self, f):
+    def mover(self, f, sonido):
         def pone(f, v):
             self.t[f.fil][f.col] = Ficha(v, f.fil, f.col)
 
@@ -97,32 +83,34 @@ class Tabla:
             pone(f, 'Y')
             self.anterior = f
 
-        def errado(n):
-            pg.mixer.music.load('no.mp3')
-            pg.mixer.music.play(0)
-            print(n)
+        def bip():
+            if sonido != 0:
+                pg.mixer.music.load('ok.mp3')
+                pg.mixer.music.play(0)
+
+        def errado():
+            if sonido != 0:
+                pg.mixer.music.load('no.mp3')
+                pg.mixer.music.play(0)
 
         # ----- MOVER
         ant = self.anterior
         d = f.valor
         if (d == "esp ") or ((d == "o") and (ant == "")):
-            errado("No hay ficha ahí")
+            errado()
             return
         if d in "xY":
             marca(f)
             return
-        if (f.col != ant.col) and (f.fil != ant.fil):
-            errado("No puedo mover diagonal")
-            return
-        if (abs(f.col - ant.col) + abs(f.fil - ant.fil)) != 2:
-            errado("No puedo saltar tan lejos")
+        if ((f.col != ant.col) and (f.fil != ant.fil)) or ((abs(f.col - ant.col) + abs(f.fil - ant.fil)) != 2):
+            errado()
             return
         if f.col == ant.col:
             salto = self.t[int((f.fil + ant.fil) / 2)][f.col]
         else:
             salto = self.t[f.fil][int((f.col + ant.col) / 2)]
         if salto.valor != "x":
-            errado("Debe haber una ficha al medio")
+            errado()
             return
         else:
             pone(ant, 'o')
@@ -132,37 +120,39 @@ class Tabla:
             self.fichas -= 1
             self.comidas += 1
             self.puntaje += 100
-            if sonido != 0:
-                pg.mixer.music.load('ok.mp3')
-                pg.mixer.music.play(0)
+            bip()
             if self.fichas == 1:
                 return "Next"
 
 
-def jugar(n):
+def jugar(n, puntos, sonido):
     pantalla.blit(fondo, (0, 0))
-    mensaje("NIVEL " + str(n), 350, 450, 2000)
-    senku = Tabla(niveles[n])
-
+    mensaje("NIVEL " + str(n), 350, 450, 1000)
+    senku = Tabla(niveles[n], puntos)
     senku.redibujar()
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                return "salir"
+                return "salir", senku.puntaje
             if event.type == pg.MOUSEBUTTONUP:
                 pos = pg.mouse.get_pos()
                 for fila in senku.t:
                     for f in fila:
                         if f.rc.collidepoint(pos):
                             if f.valor != "esp":
-                                m = senku.mover(f)
+                                m = senku.mover(f, sonido)
+                                print(sonido)
                                 if m == "Next":
-                                    return "Next"
+                                    return "Next", senku.puntaje
                                 senku.redibujar()
-            keys = pg.key.get_pressed()
-            if keys[pg.K_s]:
-                return "s"
+                if salida.get_rect(topleft=(920, 700)).collidepoint(pos):
+                    return "salir", senku.puntaje
+                if reiniciar.get_rect(topleft=(740, 700)).collidepoint(pos):
+                    return "r", senku.puntaje
+                if ruido.get_rect(topleft=(830, 700)).collidepoint(pos):
+                    sonido = 0
+                if silencio.get_rect(topleft=(830, 700)).collidepoint(pos):
+                    sonido = 1
         pg.display.flip()
 
 
@@ -178,14 +168,44 @@ def mensaje(t, x, y, tiempo: int = 0):
 
 def felicitaciones(n):
     pg.draw.rect(pantalla, Negro, (60, 250, 580, 500))
-    mensaje("FELICITACIONES II", 350, 350)
-    mensaje("HAS VENCIDO EL NIVEL " + str(n), 350, 450)
+    pg.draw.rect(pantalla, Negro, (710, 240, 280, 520))
+    mensaje("NIVEL " + str(n) + " VENCIDO II", 350, 350)
     bono = (n + 1) * 100
-    mensaje("RECOMPENSA " + str(bono) + " PUNTOS", 350, 550, 3000)
+    mensaje("RECOMPENSA " + str(bono) + " PUNTOS", 350, 450)
+    mensaje("FELICITACIONES", 350, 550, 2000)
 
+
+# Parte gráfica
+pg.init()
+pantalla = pg.display.set_mode((1090, 800))
+pg.display.set_caption('SENKU - La revancha')
+# Imágenes
+splash = pg.image.load('splash.png')
+fondo = pg.image.load('fondo.png')
+ruido = pg.image.load('sonido.gif')
+silencio = pg.image.load('silencio.gif')
+reiniciar = pg.image.load('reiniciar.gif')
+salida = pg.image.load('salida.gif')
+# Fichas
+anchoficha = 60
+altoficha = 60
+x = pg.image.load('x.png')
+o = pg.image.load('o.png')
+Y = pg.image.load('Y.png')
+esp = pg.image.load('esp.png')
+# Textos
+fuente36 = pg.font.Font('8-BIT-WONDER.TTF', 36)
+fuente24 = pg.font.Font('8-BIT-WONDER.TTF', 24)
+# Colores
+Negro = (0, 0, 0)
+Amarillo = (206, 206, 0)
+Violeta = (206, 0, 206)
+# Variables
+nivel = 0
+sonido = 1
+puntos = 0
 
 niveles = []
-niveles.append("       ,       , xxo   ,       ,       ,       ,       ")
 niveles.append("       ,   x   , xxo   ,   o   ,       ,       ,       ")
 niveles.append("  oxo  ,  xoxo ,oxoxo  ,xox    ,oxoxo  , ox x  ,    oxx")
 niveles.append("  ooo  , oxxo  ,xxooo  ,  xox  ,  ooo  , oooxx , xxooo ")
@@ -203,18 +223,19 @@ niveles.append(" oxo  x, xox  x, ooxoxo, xxxoxo, xoooox,    xxo,       ")
 niveles.append("     x , oxoxo , oxoxx , oxxox , xoxxo ,xxoxxxx,       ")
 niveles.append("  xxx  ,  xxx  ,xxxxxxx,xxx0xxx,xxxxxxx,  xxx  ,  xxx  ")
 
-# Parte gráfica
-pantalla = pg.display.set_mode((1090, 800))
-pg.display.set_caption('SENKU - La revancha')
-nivel = 0
-sonido = 0
+pantalla.blit(splash, (0, 0))
+pg.display.flip()
+pg.time.delay(2000)
 
 while True:
-    r = jugar(nivel)
+    r, puntos = jugar(nivel, puntos, sonido)
     if r == "Next":
         felicitaciones(nivel)
+        puntos += nivel * 100
         nivel += 1
     elif r == "salir":
-        print("El SENKU te ha vencido!")
+        pantalla.blit(fondo, (0, 0))
+        mensaje("El SENKU TE HA VENCIDO", 350, 450, 1000)
         break
+    print(puntos)
 pg.quit()
